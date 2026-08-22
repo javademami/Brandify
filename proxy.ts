e602bab886da705114ff40e48192758b2d8d9c4c
javademami <javademami2030@gmail.com>
2026-08-22 12:01:45 +0200

    fix nextjs proxy middleware

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,1 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-  ],
-};
+import { clerkMiddleware } from "@clerk/nextjs/server"; export default clerkMiddleware(); export const config = { matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"], };
